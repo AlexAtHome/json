e5c2d5690cf7691b167209ef789606591b7474f6
Alex Bolotskov <mamoruuu@proton.me>
2023-03-25 17:43:19 +0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormatterComponent } from './components/formatter/formatter.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterOutlet, FormatterComponent]
 })
 export class AppComponent {
-  title = 'json';
 }
---
+++ src/app/components/formatter/formatter.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formatter',
+  templateUrl: './formatter.component.html',
+  styleUrls: ['./formatter.component.scss'],
+	standalone: true,
+	imports: [CommonModule]
+})
+export class FormatterComponent {
+
+}
