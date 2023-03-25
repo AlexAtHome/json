ac545e1f2c705a20e1a656b6cc2a6c0eb99d3ee4
Alex Bolotskov <mamoruuu@proton.me>
2023-03-25 18:23:42 +0600

    feat: 🎸 barebones json formatter

--- src/app/components/formatter/formatter.component.ts
+++ src/app/components/formatter/formatter.component.ts
@@ -1,13 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HighlightModule } from 'ngx-highlightjs';
 
 @Component({
-  selector: 'app-formatter',
-  templateUrl: './formatter.component.html',
-  styleUrls: ['./formatter.component.scss'],
+	selector: 'app-formatter',
+	templateUrl: './formatter.component.html',
+	styleUrls: ['./formatter.component.scss'],
 	standalone: true,
-	imports: [CommonModule]
+	imports: [CommonModule, FormsModule, HighlightModule],
 })
 export class FormatterComponent {
+	protected input = JSON.stringify({
+		foo: 'bar'
+	})
 
+	get output() {
+		return JSON.parse(this.input)
+	}
 }
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,20 @@ import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
+	providers: [
+		importProvidersFrom(BrowserModule, AppRoutingModule),
+		{
+			provide: HIGHLIGHT_OPTIONS,
+			useValue: {
+				fullLibraryLoader: () => import('highlight.js'),
+				// languages: {
+				// 	json: () => import('highlight.js/lib/languages/json'),
+				// }
+			}
+		}
+	]
 })
-  .catch(err => console.error(err));
+	.catch(err => console.error(err));
