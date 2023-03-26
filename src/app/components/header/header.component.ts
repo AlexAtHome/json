324a32018a845f15c81fc6ce3ee38a446b1f0f3f
Alex Bolotskov <mamoruuu@proton.me>
2023-03-27 01:39:29 +0600

    feat: 🎸 adds a link to github

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+	imports: [NgxBootstrapIconsModule],
+	host: {
+		class: 'flex relative p-2 sm:p-4 items-center justify-between',
+	}
 })
 export class HeaderComponent {
-
+	readonly githubUrl = 'https://github.com/AlexAtHome/json'
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { clipboard, indent, textIndentLeft, NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { clipboard, indent, textIndentLeft, github, NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
 
 bootstrapApplication(AppComponent, {
-	providers: [importProvidersFrom(BrowserModule, NgxBootstrapIconsModule.pick({ clipboard, indent, textIndentLeft }))]
+	providers: [importProvidersFrom(BrowserModule, NgxBootstrapIconsModule.pick({ clipboard, indent, textIndentLeft, github }))]
 })
 	.catch(err => console.error(err));
