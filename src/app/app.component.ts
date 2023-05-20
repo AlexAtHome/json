22af74901dc028c73eb28574d405d07e4dddda13
Alex Bolotskov <mamoruuu@proton.me>
2023-05-20 23:04:31 +0600

    feat: footer component with privacy policy link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '@components/footer';
 import { HeaderComponent } from '@components/header';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet],
 	host: {
 		class: 'flex flex-col h-screen'
 	}
 })
-export class AppComponent {
-	protected readonly currentYear = new Date().getFullYear();
-}
+export class AppComponent {}
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './footer.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+	host: {
+		class: 'flex justify-between items-center text-black dark:text-white font-sans p-4'
+	}
+})
+export class FooterComponent {
+	protected readonly currentYear = new Date().getFullYear();
+}
