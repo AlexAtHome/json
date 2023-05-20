234402bacd530a496f6b77ecca7ee3a560697791
Alex Bolotskov <mamoruuu@proton.me>
2023-05-20 23:08:04 +0600

    feat: adds a link to the formatter itself to a header 'logo'

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,11 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-footer',
-  standalone: true,
-  imports: [RouterLink],
-  templateUrl: './footer.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: 'app-footer',
+	standalone: true,
+	imports: [RouterLink],
+	templateUrl: './footer.component.html',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 	host: {
 		class: 'flex justify-between items-center text-black dark:text-white font-sans p-4'
 	}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  templateUrl: './header.component.html',
-	imports: [NgxBootstrapIconsModule],
+	selector: 'app-header',
+	standalone: true,
+	templateUrl: './header.component.html',
+	imports: [NgxBootstrapIconsModule, RouterLink],
 	host: {
 		class: 'flex relative p-4 items-center justify-between'
 	}
