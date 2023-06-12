9b0a16713b71ed7839ba4908218b47e2f9ec1791
Alex Bolotskov <mamoruuu@proton.me>
2023-06-12 20:52:12 +0600

    proof of concept frontend update check

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,44 @@
-import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core'
+import { NgIf } from '@angular/common'
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+import { ChangeDetectionStrategy, Component, HostBinding, OnInit, signal } from '@angular/core'
 import { RouterLink } from '@angular/router'
+import { distinctUntilKeyChanged, filter, fromEvent, iif, interval, retry, switchMap, take, timer } from 'rxjs'
+import { environment } from 'src/environments/environment'
 
 @Component({
 	selector: 'app-footer',
 	standalone: true,
-	imports: [RouterLink],
+	imports: [RouterLink, NgIf, HttpClientModule],
 	templateUrl: './footer.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 	@HostBinding('class') readonly className =
-		'flex justify-between items-center text-black dark:text-white font-sans p-4'
+		'flex gap-4 justify-between items-center p-4 font-sans text-black dark:text-white'
 	protected readonly currentYear = new Date().getFullYear()
+	protected readonly version = environment.version
+	protected readonly isUpdateAvailable = signal(false)
+
+	constructor(private readonly http: HttpClient) {}
+
+	ngOnInit(): void {
+		const delay = 5000
+		interval(delay)
+			.pipe(
+				switchMap(() => this.http.get<typeof environment>('/version.json')),
+				distinctUntilKeyChanged('version'),
+				filter((response) => response.version !== this.version),
+				retry({
+					delay: () =>
+						iif(() => navigator.onLine, timer(delay), fromEvent(window, 'online', { once: true })).pipe(take(1)),
+				})
+			)
+			.subscribe(() => {
+				this.isUpdateAvailable.set(true)
+			})
+	}
+
+	reload(): void {
+		location.reload()
+	}
 }
