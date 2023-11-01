c00d2ce436a1db6d7b78b2b9eb6139a8e05819bc
Alexander Bolotskov <a.bolotskov@jusan.kz>
2023-11-01 17:10:25 +0600

    feat: service worker updater

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,14 @@
-import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core'
+import { NgIf } from '@angular/common'
+import { ChangeDetectionStrategy, Component, HostBinding, signal } from '@angular/core'
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 import { RouterLink } from '@angular/router'
+import { SwUpdate } from '@angular/service-worker'
+import { filter } from 'rxjs'
 
 @Component({
 	selector: 'app-footer',
 	standalone: true,
-	imports: [RouterLink],
+	imports: [RouterLink, NgIf],
 	templateUrl: './footer.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -12,4 +16,28 @@ export class FooterComponent {
 	@HostBinding('attr.class') readonly className =
 		'flex justify-between items-center text-black dark:text-white font-sans p-4'
 	protected readonly currentYear = new Date().getFullYear()
+
+	protected readonly isUpdateButtonVisible = signal(false)
+
+	constructor(private readonly swUpdate: SwUpdate) {
+		swUpdate.versionUpdates
+			.pipe(
+				filter((event) => event.type === 'VERSION_READY'),
+				takeUntilDestroyed()
+			)
+			.subscribe(() => {
+				this.isUpdateButtonVisible.set(true)
+			})
+	}
+
+	protected async activateUpdate(): Promise<void> {
+		try {
+			const result = await this.swUpdate.activateUpdate()
+			if (result) {
+				location.reload()
+			}
+		} catch (error) {
+			console.error(error)
+		}
+	}
 }
