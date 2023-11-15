2c06c4a50730446695eab4ba50584f753030d075
Alex Bolotskov <mamoruuu@proton.me>
2023-11-15 22:31:02 +0600

    chore: runs tests on every commit

--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { FooterComponent } from './footer.component'
 import { RouterTestingModule } from '@angular/router/testing'
+import { provideServiceWorker } from '@angular/service-worker'
 
 describe('FooterComponent', () => {
 	let component: FooterComponent
@@ -10,6 +11,11 @@ describe('FooterComponent', () => {
 	beforeEach(() => {
 		TestBed.configureTestingModule({
 			imports: [RouterTestingModule, FooterComponent],
+			providers: [
+				provideServiceWorker('sw-test.js', {
+					enabled: false,
+				}),
+			],
 		})
 		fixture = TestBed.createComponent(FooterComponent)
 		component = fixture.componentInstance
