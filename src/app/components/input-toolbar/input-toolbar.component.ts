2a07b95073d95d86084e09d979ac041f9f1e0259
Alex Bolotskov <mamoruuu@proton.me>
2025-04-17 18:20:09 +0500

    feat: xslt processor

--- src/app/components/input-toolbar/input-toolbar.component.ts
+++ src/app/components/input-toolbar/input-toolbar.component.ts
@@ -1,12 +1,15 @@
 import { ChangeDetectionStrategy, Component, model } from '@angular/core'
 import { FormsModule } from '@angular/forms'
+import { SelectComponent } from '@components/select'
 
 @Component({
 	selector: 'app-input-toolbar',
 	templateUrl: './input-toolbar.component.html',
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	imports: [FormsModule],
+	imports: [FormsModule, SelectComponent],
 })
 export class InputToolbarComponent {
 	readonly decodeBase64 = model(false)
+	readonly format = model('json')
+	readonly formats = ['json', 'xml']
 }
--- src/app/pages/formatter/formatter.component.ts
+++ src/app/pages/formatter/formatter.component.ts
@@ -1,10 +1,10 @@
-import { ChangeDetectionStrategy, Component, computed, model, signal } from '@angular/core'
+import { ChangeDetectionStrategy, Component, computed, inject, model, signal } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { InputToolbarComponent } from '@components/input-toolbar/input-toolbar.component'
 import { OutputToolbarComponent } from '@components/output-toolbar'
 import copy from '@func/copy'
 import download from '@func/download'
-import { formatJson } from '@func/format-json'
+import { formatJson, FormatJsonOutput } from '@func/format-json'
 import { isEqual } from '@func/is-equal'
 import type { IndentSize, IndentType } from '@interfaces/json.interface'
 
@@ -23,6 +23,7 @@ export class FormatterComponent {
 	readonly indentSize = signal<IndentSize>(2, { equal: isEqual })
 	protected readonly input = model<string>('')
 	protected readonly decodeBase64 = model(false)
+	protected readonly format = signal('json')
 
 	protected readonly decodedValue = computed(() => {
 		if (this.decodeBase64()) {
@@ -33,6 +34,17 @@ export class FormatterComponent {
 	})
 
 	readonly output = computed(() => {
+		if (this.format() === 'json') {
+			return this.getJsonOutput()
+		} else {
+			return this.getXmlOutput()
+		}
+	})
+
+	private readonly domParser = new DOMParser()
+	private readonly xslt = this.getXSLTProcessor()
+
+	private getJsonOutput(): FormatJsonOutput {
 		try {
 			const size = Number(this.indentSize()) as IndentSize
 			// TODO: Fix the select component so it returns a number when it's passed a list of numbers
@@ -43,7 +55,28 @@ export class FormatterComponent {
 				error: (e as Error).message,
 			}
 		}
-	})
+	}
+
+	getXmlOutput(): FormatJsonOutput {
+		if (!this.decodedValue().trim()) {
+			return {
+				data: '',
+			}
+		}
+		const xmlRef = document.implementation.createDocument('', '', null)
+		const node = this.domParser.parseFromString(this.decodedValue(), 'application/xml')
+		const fragment = this.xslt.transformToFragment(node, xmlRef)
+		const child = fragment.children[0]
+		if (child.localName === 'parsererror') {
+			return {
+				data: this.decodedValue(),
+				error: fragment.children[0].textContent ?? undefined,
+			}
+		}
+		return {
+			data: child.outerHTML,
+		}
+	}
 
 	protected copy(): void {
 		copy(this.output().data)
@@ -55,5 +88,22 @@ export class FormatterComponent {
 
 	protected reset(): void {
 		this.input.set('')
+	}
+
+	private getXSLTProcessor(): XSLTProcessor {
+		const xsltProcessor = new XSLTProcessor()
+		const xslt = `
+			<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+				<xsl:output omit-xml-declaration="yes" indent="yes"/>
+				<xsl:template match="node()|@*">
+					<xsl:copy>
+						<xsl:apply-templates select="node()|@*"/>
+					</xsl:copy>
+				</xsl:template>
+			</xsl:stylesheet>
+		`
+		const xslStylesheet = this.domParser.parseFromString(xslt, 'application/xml')
+		xsltProcessor.importStylesheet(xslStylesheet)
+		return xsltProcessor
 	}
 }
